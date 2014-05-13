b61a082df9454320494c3422a9e2baffcb793743
Robert Warren <grindnmosh@gmail.com>
2014-05-13 10:33:48 -0700

    catch up on uncomitted code for week 2

---
+++ Week2/J1Demo/J1Demo/src/main/java/com/grinddesign/Java1/j1demo/j1demo/MainActivity.java
@@ -0,0 +1,24 @@
+//Robert Warren
+//Java 1
+//Term 1405
+
+package com.grinddesign.Java1.j1demo.j1demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
