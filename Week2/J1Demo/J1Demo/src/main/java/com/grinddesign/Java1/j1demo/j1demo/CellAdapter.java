176d3839a188f8acd911745d454c4cf5e5177ceb
Robert Warren <grindnmosh@gmail.com>
2014-05-13 19:31:00 -0700

    catching up on changes and sample data. ENUMs still elude me

--- Week2/J1Demo/J1Demo/src/main/java/com/grinddesign/Java1/j1demo/j1demo/CellAdapter.java
+++ Week2/J1Demo/J1Demo/src/main/java/com/grinddesign/Java1/j1demo/j1demo/CellAdapter.java
@@ -1,13 +1,16 @@
 package com.grinddesign.Java1.j1demo.j1demo;
 
-/**
- * Created by 1LoveCyn on 5/13/14.
- */
-
 //Robert Warren
 //Java 1
 //Term 1405
 
-public class CellAdapter {
+/**
+ * Created by 1LoveCyn on 5/13/14.
+ */
 
+
+public class CellAdapter {
+    public CellAdapter() {
+
+    }
 }
--- Week2/J1Demo/J1Demo/src/main/java/com/grinddesign/Java1/j1demo/j1demo/FCellAdapter.java
+++ Week2/J1Demo/J1Demo/src/main/java/com/grinddesign/Java1/j1demo/j1demo/FCellAdapter.java
@@ -1,7 +1,14 @@
 package com.grinddesign.Java1.j1demo.j1demo;
 
+//Robert Warren
+//Java 1
+//Term 1405
+
 /**
  * Created by 1LoveCyn on 5/13/14.
  */
 public class FCellAdapter {
+    public FCellAdapter() {
+
+    }
 }
